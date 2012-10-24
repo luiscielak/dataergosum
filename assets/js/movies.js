ab96803349485322a231a7c82fcf041b3c38e6e2
Luis Cielak <luis.cielak@gmail.com>
2012-10-24 13:45:16 -0400

    initial file upload

---
+++ assets/js/movies.js
@@ -0,0 +1,77 @@
+var films = 
+[
+{
+  "code": "/api/status/ok",
+  "result": {
+    "country": [
+      "United Kingdom",
+      "United States of America"
+    ],
+    "directed_by": [
+      "Stanley Kubrick"
+    ],
+    "genre": [
+      "Dystopia",
+      "Satire",
+      "Black comedy",
+      "Science Fiction",
+      "Crime Fiction",
+      "New Hollywood",
+      "Cult",
+      "Crime",
+      "Juvenile Delinquency Film",
+      "Drama"
+    ],
+    "id": "/en/a_clockwork_orange_1971",
+    "initial_release_date": [
+      "1971-12-19"
+    ],
+    "language": [
+      "English Language"
+    ],
+    "music": [
+      "Wendy Carlos",
+      "Rachel Elkind-Tourre"
+    ],
+    "name": "A Clockwork Orange",
+    "rating": [
+      "R (USA)"
+    ],
+    "runtime": [
+      null,
+      null
+    ],
+    "story_by": [
+      "Anthony Burgess"
+    ],
+    "tagline": [
+      "Being the adventures of a young man whose principal interests are rape, ultra-violence and Beethoven.",
+      "The Breakthrough Presentation Of Stanley Kubrick's Clockwork Orange For The Millions Who Were Not Allowed To See It Until Now!"
+    ],
+    "type": "/film/film"
+  },
+  "status": "200 OK",
+  "transaction_id": "cache;cache01.p01.sjc1:8101;2012-03-06T00:45:47Z;0050"
+}
+];
+
+var lobs =
+[
+{"RptPrd":200905,"LOB_12":null,"LOB_10":25,"LOB_6":null,"LOB_7":99,"LOB_4":301,"LOB_3":null,"LOB_2":679,"LOB_1":627,"LOB_8":120,"LOB_11":null,"LOB_9":65,"LOB_5":281},
+{"RptPrd":200906,"LOB_12":null,"LOB_10":24,"LOB_6":null,"LOB_7":100,"LOB_4":299,"LOB_3":null,"LOB_2":676,"LOB_1":629,"LOB_8":116,"LOB_11":null,"LOB_9":62,"LOB_5":270},
+{"RptPrd":200907,"LOB_12":3,"LOB_10":24,"LOB_6":null,"LOB_7":110,"LOB_4":339,"LOB_3":null,"LOB_2":793,"LOB_1":650,"LOB_8":130,"LOB_11":null,"LOB_9":74,"LOB_5":276},
+{"RptPrd":200908,"LOB_12":3,"LOB_10":24,"LOB_6":null,"LOB_7":110,"LOB_4":331,"LOB_3":null,"LOB_2":765,"LOB_1":637,"LOB_8":111,"LOB_11":null,"LOB_9":71,"LOB_5":276},
+{"RptPrd":200909,"LOB_12":4,"LOB_10":24,"LOB_6":null,"LOB_7":107,"LOB_4":332,"LOB_3":null,"LOB_2":762,"LOB_1":628,"LOB_8":113,"LOB_11":null,"LOB_9":68,"LOB_5":276},
+{"RptPrd":200910,"LOB_12":4,"LOB_10":24,"LOB_6":167,"LOB_7":124,"LOB_4":328,"LOB_3":411,"LOB_2":739,"LOB_1":611,"LOB_8":110,"LOB_11":null,"LOB_9":56,"LOB_5":276},
+{"RptPrd":200911,"LOB_12":4,"LOB_10":24,"LOB_6":159,"LOB_7":124,"LOB_4":328,"LOB_3":400,"LOB_2":732,"LOB_1":611,"LOB_8":110,"LOB_11":null,"LOB_9":56,"LOB_5":276},
+{"RptPrd":200912,"LOB_12":4,"LOB_10":19,"LOB_6":155,"LOB_7":122,"LOB_4":281,"LOB_3":395,"LOB_2":703,"LOB_1":595,"LOB_8":108,"LOB_11":null,"LOB_9":52,"LOB_5":265},
+{"RptPrd":201001,"LOB_12":4,"LOB_10":19,"LOB_6":146,"LOB_7":125,"LOB_4":277,"LOB_3":389,"LOB_2":642,"LOB_1":581,"LOB_8":108,"LOB_11":null,"LOB_9":49,"LOB_5":269},
+{"RptPrd":201002,"LOB_12":4,"LOB_10":12,"LOB_6":147,"LOB_7":132,"LOB_4":286,"LOB_3":388,"LOB_2":641,"LOB_1":577,"LOB_8":109,"LOB_11":null,"LOB_9":49,"LOB_5":270},
+{"RptPrd":201003,"LOB_12":6,"LOB_10":9,"LOB_6":148,"LOB_7":135,"LOB_4":323,"LOB_3":366,"LOB_2":634,"LOB_1":570,"LOB_8":108,"LOB_11":null,"LOB_9":51,"LOB_5":251},
+{"RptPrd":201004,"LOB_12":5,"LOB_10":8,"LOB_6":149,"LOB_7":145,"LOB_4":323,"LOB_3":369,"LOB_2":622,"LOB_1":577,"LOB_8":106,"LOB_11":null,"LOB_9":54,"LOB_5":256},
+{"RptPrd":201005,"LOB_12":8,"LOB_10":9,"LOB_6":145,"LOB_7":142,"LOB_4":324,"LOB_3":375,"LOB_2":612,"LOB_1":595,"LOB_8":108,"LOB_11":null,"LOB_9":59,"LOB_5":261},
+{"RptPrd":201006,"LOB_12":8,"LOB_10":13,"LOB_6":149,"LOB_7":131,"LOB_4":322,"LOB_3":373,"LOB_2":626,"LOB_1":622,"LOB_8":108,"LOB_11":null,"LOB_9":58,"LOB_5":261},
+{"RptPrd":201007,"LOB_12":7,"LOB_10":13,"LOB_6":148,"LOB_7":130,"LOB_4":329,"LOB_3":371,"LOB_2":615,"LOB_1":623,"LOB_8":106,"LOB_11":null,"LOB_9":52,"LOB_5":262},
+{"RptPrd":201008,"LOB_12":8,"LOB_10":14,"LOB_6":147,"LOB_7":133,"LOB_4":332,"LOB_3":379,"LOB_2":600,"LOB_1":637,"LOB_8":107,"LOB_11":1,"LOB_9":53,"LOB_5":259},
+{"RptPrd":201009,"LOB_12":1,"LOB_10":13,"LOB_6":146,"LOB_7":134,"LOB_4":337,"LOB_3":381,"LOB_2":606,"LOB_1":655,"LOB_8":114,"LOB_11":1,"LOB_9":52,"LOB_5":251}
+];
---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* JS Variables */
+
+var color = "red";
+
+var myDiv = document.getElementById('myDiv');
+myDiv.style.background = "black";
+myDiv.style.color="white";
+
+
+console.log(myDiv);
+
+console.log(typeof myDiv);
